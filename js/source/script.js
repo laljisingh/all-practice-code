9423e8c7b8c7e3e914c21a7a85aae8e71274609f
win2@Laljisingh <lalji7411@gmail.com>
2022-05-26 07:09:49 +0530

    travel yaari

---
+++ js/source/script.js
@@ -0,0 +1,23 @@
+var a = 50;
+console.log(a);
+
+a = 40;
+a = 20;
+console.log(a);
+
+var a;
+a = 10;
+console.log(a);
+a = 20;
+console.log(a);
+a = 30;
+console.log(a);
+
+const pie = 3.14;
+console.log(typeof pie);
+//   pie = 3.1422;
+console.log(pie);
+var a=10;
+var se = "jdhdhduedu"
+alert(se);  
+console.log(se);
